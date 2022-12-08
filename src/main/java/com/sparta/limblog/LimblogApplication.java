8a3a9e4166597f60b1853fee5a0d3b012ee04f31
Lim DongGyu <ldg1160296@gmail.com>
2022-12-08 23:47:16 +0900

    Initial commit

---
+++ src/main/java/com/sparta/limblog/LimblogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.limblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimblogApplication.class, args);
+    }
+
+}
